5e6cfefc2e51dc355dbb5037eb170c9c70affefb
coelho-glith <coelho.soldado7@gmail.com>
2025-03-25 18:09:42 -0300

    Implementado Mostrador De Diamante

--- Diamante-X/Program.cs
+++ Diamante-X/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Escreva um número impar: ");
+            int tamanho = Convert.ToInt32(Console.ReadLine());
+
+            int meio = tamanho / 2;
+
+            for (int i = 0; i <= meio; i++)
+            {
+                string linha = $"{new string(' ', meio - i)}{new string('x', 2 * i + 1)}";
+                Console.WriteLine(linha);
+            }
+
+            for (int i = meio - 1; i >= 0; i--)
+            {
+                string linha = $"{new string(' ', meio - i)}{new string('x', 2 * i + 1)}";
+                Console.WriteLine(linha);
+            }
         }
     }
 }
