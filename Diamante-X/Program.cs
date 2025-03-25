01b95c57e986aec940bd48ef814976e5436ac4ed
coelho-glith <coelho.soldado7@gmail.com>
2025-03-25 19:08:27 -0300

    Implementado Loop de repetição

--- Diamante-X/Program.cs
+++ Diamante-X/Program.cs
@@ -6,31 +6,41 @@ namespace Diamante_X
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Escreva um número impar: ");
-            if (!int.TryParse(Console.ReadLine(), out int tamanho))
+            while (true)
             {
-                Console.WriteLine("Número Inteiro Invalido, Pressione novamente para continuar");
-                Console.ReadLine();
-            }
-            
-            if (tamanho % 2 == 0)
-            {
-                Console.WriteLine("Número informado não é ímpar. Pressione novamente para continuar");
-                Console.ReadLine();
-            }
 
-            int meio = tamanho / 2;
+                Console.WriteLine("Escreva um número impar: ");
+                if (!int.TryParse(Console.ReadLine(), out int tamanho))
+                {
+                    Console.WriteLine("Número Inteiro Invalido, Pressione novamente para continuar");
+                    Console.ReadLine();
+                }
 
-            for (int i = 0; i <= meio; i++)
-            {
-                string linha = $"{new string(' ', meio - i)}{new string('x', 2 * i + 1)}";
-                Console.WriteLine(linha);
-            }
+                if (tamanho % 2 == 0)
+                {
+                    Console.WriteLine("Número informado não é ímpar. Pressione novamente para continuar");
+                    Console.ReadLine();
+                }
 
-            for (int i = meio - 1; i >= 0; i--)
-            {
-                string linha = $"{new string(' ', meio - i)}{new string('x', 2 * i + 1)}";
-                Console.WriteLine(linha);
+                int meio = tamanho / 2;
+
+                for (int i = 0; i <= meio; i++)
+                {
+                    string linha = $"{new string(' ', meio - i)}{new string('x', 2 * i + 1)}";
+                    Console.WriteLine(linha);
+                }
+
+                for (int i = meio - 1; i >= 0; i--)
+                {
+                    string linha = $"{new string(' ', meio - i)}{new string('x', 2 * i + 1)}";
+                    Console.WriteLine(linha);
+                }
+
+                Console.Write("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
             }
         }
     }
